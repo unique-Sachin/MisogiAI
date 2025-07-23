0b9f18603c128bbd6f745483c289a47bcbbc29ef
Sachin Mishra <sachin.mern@gmail.com>
2025-07-23 14:31:23 +0530

    updated

---
+++ Intro-to-Python/Q12.py
@@ -0,0 +1,9 @@
+name = input("Enter you full name:")
+age = input("Enter you age:")
+city = input("Enter your city:")
+hobby = input("Enter you hobby:")
+
+print(f"""Hello, {name}!
+You are {age} years old and live in city {city}.
+In your free time you enjoy {hobby}
+""")
